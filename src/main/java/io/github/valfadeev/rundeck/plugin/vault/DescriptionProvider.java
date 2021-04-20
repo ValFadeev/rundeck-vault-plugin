9f31662cbfa6b44c11a513d6cd3b1b3f08996734
Forrest Evans <fdevans@gmail.com>
2021-04-20 16:23:38 -0700

    Update Plugin Name/Descrription

--- src/main/java/io/github/valfadeev/rundeck/plugin/vault/DescriptionProvider.java
+++ src/main/java/io/github/valfadeev/rundeck/plugin/vault/DescriptionProvider.java
@@ -12,8 +12,8 @@ class DescriptionProvider {
     static Description getDescription() {
         return DescriptionBuilder.builder()
                 .name("vault-storage")
-                .description("Stores data in Vault")
-                .title("Vault storage")
+                .description("Use Hashicorp Vault secrets as backend for Rundeck Key Storage")
+                .title("Hashicorp Vault - Key Storage Plugin")
                 .property(PropertyBuilder.builder()
                         .string(VAULT_PREFIX)
                         .title("Vault prefix")
